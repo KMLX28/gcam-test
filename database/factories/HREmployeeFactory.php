b07e5c6915703f6db575065ff46c2d9fac36972f
Muhammed <muhamdkudiry@gmail.com>
2022-01-08 17:26:46 +0300

    first

--- database/factories/HREmployeeFactory.php
+++ database/factories/HREmployeeFactory.php
@@ -13,7 +13,7 @@ class HREmployeeFactory extends Factory
   {
     return [
         'name' => $this->faker->name(),
-        'email' => $this->faker->unique()->safeEmail(),
+        'email' => 'employee@mail.com',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ];
   }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,32 +2,28 @@
     <Head title="Welcome"/>
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-        <div v-if="!$page.props.auth.user?.trusted"
-             class="max-w-6xl mx-auto my-auto p-5 border-b border-indigo-400 bg-indigo-100 rounded text-indigo-800">
-            <template>
+        <div class="max-w-6xl mx-auto my-auto p-5 border-b border-indigo-400 bg-indigo-100 rounded text-indigo-800">
+            <div v-if="$page.props.auth.user" class="grid gap-4">
+                <Link  :href="route('dashboard')" class="text-sm text-gray-700 underline">
+                    <Button>
+                        الرئيسية
+                    </Button>
+                </Link>
+            </div>
 
-            </template>
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
-                <Button>
-                    الرئيسية
-                </Button>
-            </Link>
+            <div v-if="!$page.props.auth.user" class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <Link  :href="route('login')" class="text-sm text-gray-700 mx-auto">
+                    <Button>
+                        دخول
+                    </Button>
+                </Link>
 
-            <template v-else>
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <Link :href="route('login')" class="text-sm text-gray-700 mx-auto">
-                        <Button>
-                            دخول
-                        </Button>
-                    </Link>
-
-                    <Link :href="route('register')" class="text-sm text-gray-700 mx-auto">
-                        <Button>
-                            تسجيل
-                        </Button>
-                    </Link>
-                </div>
-            </template>
+                <Link :href="route('register')" class="text-sm text-gray-700 mx-auto">
+                    <Button>
+                        تسجيل
+                    </Button>
+                </Link>
+            </div>
         </div>
     </div>
 
